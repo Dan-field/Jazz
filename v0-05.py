2da627c2e13f9ca37913b20e7fbe35de50864262
Daniel Field <daniel@fieldfam.com>
2017-03-04 23:48:30 +1000

    update so beats per bar is a variable

--- v0-05.py
+++ v0-05.py
@@ -1,8 +1,18 @@
 from improfunctions import *
 from music import *
 
-chords, bars = GetChords('SatinDoll.txt')
+output = Phrase()
 
-print chords
-print bars
-print len(bars)
+# import the chord list and the bar list from file
+chords, bars = GetChords('SatinDoll.txt')
+beats_per_bar = 4.0 # must be a decimal (not int) since it may be used for division
+
+# Work through one bar at a time
+for bar_count, bar in enumerate(bars):
+   # get the root notes and durations
+   roots, durations = BreakDown(bar, beats_per_bar)
+   root_numbers = Octavify(roots, '4') # pass the MIDI octave number as a string
+   output.addNoteList(root_numbers, durations)
+   print roots, root_numbers, durations
+output.setTempo(200)
+Play.midi(output)
