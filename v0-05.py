2ce8894d4360624b3b95ce28ff25c952a751879e
Daniel Field <daniel@fieldfam.com>
2017-03-27 21:24:45 +1000

    fixed bug with 'repeat chord' (/) symbol

--- v0-05.py
+++ v0-05.py
@@ -6,14 +6,19 @@ import threading
 output = Phrase()
 
 # import the chord list and the bar list from file
-chords, bars = GetChords('SatinDoll.txt')
+chords, bars = GetChords('Majority.txt')
 beats_per_bar = 4.0 # must be a decimal (not int) since it may be used for division
+last_root = 'NC'
 
 # Work through one bar at a time
 for bar_count, bar in enumerate(bars):
    # get the root notes and durations
    roots, durations = BreakDown(bar, beats_per_bar)
-   root_numbers = Octavify(roots, '4') # pass the MIDI octave number as a string
+   if roots[-1] != 'R': # the bar contains actual chords, not a 'repeat chord' symbol
+      root_numbers = Octavify(roots, '4') # pass the MIDI octave number as a string
+      last_root = root_numbers
+   else:
+      root_numbers = last_root
    output.addNoteList(root_numbers, durations)
    print roots, root_numbers, durations
 output.setTempo(200)
