fd1fad0e820197afc9990c7511b0ee6076dc6af6
Daniel Field <daniel@fieldfam.com>
2017-04-10 21:05:24 +1000

    updated so notes are played by the timer

--- v0-05.py
+++ v0-05.py
@@ -1,27 +1,14 @@
 from improfunctions import *
 from timerfunction import *
 from music import *
-import threading
 
-output = Phrase()
+
 
 # import the chord list and the bar list from file
-chords, bars = GetChords('Majority.txt')
+chords, bars = GetChords('SatinDoll.txt')
 beats_per_bar = 4.0 # must be a decimal (not int) since it may be used for division
-last_root = 'NC'
 
-# Work through one bar at a time
-for bar_count, bar in enumerate(bars):
-   # get the root notes and durations
-   roots, durations = BreakDown(bar, beats_per_bar)
-   if roots[-1] != 'R': # the bar contains actual chords, not a 'repeat chord' symbol
-      root_numbers = Octavify(roots, '4') # pass the MIDI octave number as a string
-      last_root = root_numbers
-   else:
-      root_numbers = last_root
-   output.addNoteList(root_numbers, durations)
-   print roots, root_numbers, durations
-output.setTempo(200)
+# generate a phrase containing just the roots
+root_pitches, root_durations = ExtractRoots(bars, beats_per_bar)
 
-Play.midi(output)
-startTimer()
+startTimer(root_pitches, root_durations)
