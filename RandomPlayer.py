28ed21493ace393ffe45155261cf8a83b7b9f5ea
Daniel Field <daniel@fieldfam.com>
2017-05-03 16:25:17 +1000

    added some randomness to the note velocities

--- RandomPlayer.py
+++ RandomPlayer.py
@@ -15,15 +15,18 @@ class RandomPlayer:
       self.phr = Phrase()
       self.thisBeatRhythm = [0.5, 0.5]
       self.thisBeatNotes = [56, 57]
+      self.thisBeatVelocities = [100, 100]
       self.nextBeatRhythm = [0.5, 0.5]
       self.nextBeatNotes = [58, 59]
+      self.nextBeatVelocities = [100, 100]
       
    def playCurrentBeat(self, tempo=None):
       if tempo is None: tempo = 120
       self.phr.setTempo(tempo)
       self.thisBeatRhythm = self.nextBeatRhythm
       self.thisBeatNotes = self.nextBeatNotes
-      self.phr.addNoteList(self.thisBeatNotes, self.thisBeatRhythm)
+      self.thisBeatVelocities = self.nextBeatVelocities
+      self.phr.addNoteList(self.thisBeatNotes, self.thisBeatRhythm, self.thisBeatVelocities)
       Play.midi(self.phr)
       self.phr.empty()
    
@@ -32,8 +35,12 @@ class RandomPlayer:
       b = 1.0 - a
       c = randint(40, 80)
       d = c+randint(-5, 5)
+      e = 100 + randint(-25, 25)
+      f = e + randint(-20, 20)
+      if f > 127: f=127
       self.nextBeatRhythm = [a, b]
       self.nextBeatNotes = [c, d]
+      self.nextBeatVelocities = [e, f]
       
 
    def beat(self, tempo=None):
